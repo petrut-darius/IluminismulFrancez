f887cca3af145e74584c95007fc03e7b33346959
petrut-darius <eminoviciidarius@gmail.com>
2025-12-30 22:54:35 +0200

    backend from dashboard to home route name

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -1,27 +1,32 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold  text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Profile') }}
-        </h2>
-    </x-slot>
+    <div class="w-[70vw]">
+        <x-page-title>Profile</x-page-title>
+        <div>
+            <x-slot name="header">
+                <h2 class="font-semibold  text-xl text-gray-800 dark:text-gray-200 leading-tight">
+                    {{ __('Profile') }}
+                </h2>
+            </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
-            <div class="p-4 sm:p-8 bg-green-100/80 border rounded border-solid border-green-700 shadow sm:rounded-xl">
-                <div class="max-w-xl ">
-                    @include('profile.partials.update-profile-information-form')
-                </div>
-            </div>
+            <div>
+                <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
+                    <div class="p-4 sm:p-8 bg-green-100/80 border rounded border-solid border-green-700 shadow sm:rounded-xl">
+                        <div class="max-w-xl ">
+                            @include('profile.partials.update-profile-information-form')
+                        </div>
+                    </div>
 
-            <div class="p-4 sm:p-8 bg-green-100/80 border rounded border-solid border-green-700 shadow sm:rounded-xl">
-                <div class="max-w-xl">
-                    @include('profile.partials.update-password-form')
-                </div>
-            </div>
+                    <div class="p-4 sm:p-8 bg-green-100/80 border rounded border-solid border-green-700 shadow sm:rounded-xl">
+                        <div class="max-w-xl">
+                            @include('profile.partials.update-password-form')
+                        </div>
+                    </div>
 
-            <div class="p-4 sm:p-8 bg-green-100/80 border rounded border-solid border-green-700 shadow sm:rounded-xl">
-                <div class="max-w-xl">
-                    @include('profile.partials.delete-user-form')
+                    <div class="p-4 sm:p-8 bg-green-100/80 border rounded border-solid border-green-700 shadow sm:rounded-xl">
+                        <div class="max-w-xl">
+                            @include('profile.partials.delete-user-form')
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
